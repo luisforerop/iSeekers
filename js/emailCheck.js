e13fa50d545d78c9c5b4afb1703a710fe4643a3e
cquinchia <67036434+cquinchia@users.noreply.github.com>
2021-09-23 17:46:17 -0500

    Update emailCheck.js

--- js/emailCheck.js
+++ js/emailCheck.js
@@ -1,8 +1,12 @@
 
 const checkCorreo = (valor) => {
     console.log(`Este es el valor: ${valor}`);
-    /**CÓDIGO DE VALIDACIÓN */
-    return // verdadero o falso
+        if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+            alert("la direccion de Email "+valor+" es correcta");
+        } else {
+            alert("la direccion de Email "+valor+" no es correcta");
+            return // verdadero o falso
+        }
 }
 
 export default checkCorreo;

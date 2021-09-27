35218a21927f1550688fad18a68cb87102353490
Luis <Luis@LAPTOP-0EFHT9GS>
2021-09-27 12:05:24 -0500

    Delete unnecessary files

--- frontend/js/passwordCheck.js
+++
@@ -1,8 +0,0 @@
-
-const checkContrasena = (valor) => {
-    console.log(`Este es el valor de la contraseña: ${valor}`);
-    /**CÓDIGO DE VALIDACIÓN */
-    return // verdadero o falso
-}
-
-export default checkContrasena; 
